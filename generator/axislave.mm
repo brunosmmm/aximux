1b1cbb55f749bc6b6a2de48a3eddd0b8f5e016cd
Bruno Morais <brunosmmm@gmail.com>
2024-03-24 10:01:55 -0400

    Update to latest generator features

--- generator/axislave.mm
+++ generator/axislave.mm
@@ -1,35 +1,37 @@
 //parameters
-#register_size 32;
-#addr_mode byte;
+define register_size 32
+define addr_mode byte
 
 // generative parameters
-param mux_signals 8;
-param alt_signals 1;
-param alt_signal_size $clog2(alt_signals);
+param mux_signals 8
+param alt_signals 1
+param alt_signal_size $clog2(alt_signals)
 
 //registers
+template register SRCSEL description="Source select register" {
+  // Source selection
+  field SRC position={alt_signal_size-1}..0 access=RW description="Source select"
+  // Short selection: shorts input to incoming signals (fanout beware)
+  field SHORT position=5 access=RW description="Short input to incoming signal"
+  // Direction control enable: control direction in HW (=0) or SW (=1)
+  field DCTL position=6 access=RW description="SW direction control enable"
+  // Software Direction control
+  field DIR position=7 access=RW description="SW direction control"
+}
 
 // Selector registers
-for ii in <0, mux_signals> generate
-  register SRCSEL{ii} description="Source select register {ii}";
+for ii in <0, mux_signals> generate {
+  register SRCSEL{ii} description="Source select register {ii}" using SRCSEL
 
-  // Source selection
-  field SRCSEL{ii}.SRC position={alt_signal_size-1}..0 access=RW;
-  // Short selection: shorts input to incoming signals (fanout beware)
-  field SRCSEL{ii}.SHORT position=5 access=RW;
-  // Direction control enable: control direction in HW (=0) or SW (=1)
-  field SRCSEL{ii}.DCTL position=6 access=RW;
-  // Software Direction control
-  field SRCSEL{ii}.DIR position=7 access=RW;
-
-  output srcsel{ii} source=SRCSEL{ii}.SRC;
-  output shortsel{ii} source=SRCSEL{ii}.SHORT;
-  output diren{ii} source=SRCSEL{ii}.DCTL;
-  output dirctl{ii} source=SRCSEL{ii}.DIR;
-end;
+  output srcsel{ii} source=SRCSEL{ii}.SRC
+  output shortsel{ii} source=SRCSEL{ii}.SHORT
+  output diren{ii} source=SRCSEL{ii}.DCTL
+  output dirctl{ii} source=SRCSEL{ii}.DIR
+}
 
 // Information Registers
-register MUXINFO at 0x80 description="Hardware Information";
-field MUXINFO.SIGNALS position=7..0 access=R default={mux_signals};
-field MUXINFO.ALTSIGS position=15..8 access=R default={alt_signals};
-field MUXINFO.RESERVED position=31..16 access=R;
+register MUXINFO at 0x80 description="Hardware Information" {
+  field SIGNALS position=7..0 access=R default={mux_signals} description="Number of muxes"
+  field ALTSIGS position=15..8 access=R default={alt_signals} description="Number of sources for each mux"
+  field RESERVED position=31..16 access=R description="Reserved"
+}
